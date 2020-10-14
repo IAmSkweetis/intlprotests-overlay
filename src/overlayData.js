2a282fc97c869af9e3eea32c41f8dbb9a6a99197
Scott Wolfe <skweetis@gmail.com>
2020-10-13 20:58:23 -0700

    testing2

--- src/overlayData.js
+++ src/overlayData.js
@@ -1,5 +1,5 @@
 module.exports = {
-  baseURL: 'https://intl.litterbox.io/overlays/',
+  baseURL: 'https://intl.litterbox.io/test/',
   overlays: {
     'overlay.html': { kind: 'overlay' },
     'background.html': { kind: 'background' },
